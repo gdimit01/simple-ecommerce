aa203b87f14461c1faaafd781a015bf4a7b2f3ca
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2024-07-06 01:59:14 +0100

    fix: pathing issue fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/simple-ecommerce/", // Ensure this matches your repository name
   plugins: [react()],
   css: {
     postcss: "./postcss.config.js",
